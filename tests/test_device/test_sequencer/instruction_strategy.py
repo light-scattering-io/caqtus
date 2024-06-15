a12666a646cb83d2e279bc4514e24b55ab467941
damien <damien.bloch@institutoptique.fr>
2024-06-15 16:51:20 +0200

    Add strategy to generate sequencer instruction recursively

---
+++ tests/test_device/test_sequencer/instruction_strategy.py
@@ -0,0 +1,18 @@
+from typing import TypeVar
+
+from hypothesis.strategies import SearchStrategy, recursive, one_of
+from numpy.typing import DTypeLike
+
+from caqtus.device.sequencer.instructions import SequencerInstruction
+from .generate_concatenate import concatenation
+from .generate_pattern import pattern
+from .generate_repeat import repeated
+
+T = TypeVar("T", bound=DTypeLike)
+
+
+def instruction(dtype: T) -> SearchStrategy[SequencerInstruction[T]]:
+    return recursive(
+        pattern(dtype=dtype, min_length=1),
+        lambda s: one_of(concatenation(s), repeated(s)),
+    )
